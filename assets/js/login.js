abc483ee14f45e6bc18511d4d30d88174407e0d1
La svedese <5554930+sarasjodin@users.noreply.github.com>
2026-06-22 22:17:36 +0200

    reset form before redirect

--- assets/js/login.js
+++ assets/js/login.js
@@ -30,6 +30,7 @@ loginForm.addEventListener('submit', async (event) => {
     }
 
     localStorage.setItem('token', data.token);
+    loginForm.reset();
     window.location.href = 'profile.html'; // redirect to profile page after login
   } catch (error) {
     console.error('Login error:', error);
