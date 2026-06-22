2438c7dc31840f99c3f26be5c8361f2a98f42c0a
La svedese <5554930+sarasjodin@users.noreply.github.com>
2026-06-23 00:10:59 +0200

    add logout functionality to btn

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,29 @@
 import { setupMenu } from './menu.js'; // Imports function that handles the hb menu and logout button
+
+const body = document.querySelector('body');
+
+async function checkAuth() {
+  const token = localStorage.getItem('token'); // Checks local storage for token, if not found, user is logged out
+
+  if (!token) {
+    setLoggedOut();
+    return;
+  }
+
+  setLoggedIn();
+}
+
+/*
+Update pages states  */
+function setLoggedIn() {
+  body.classList.add('is-logged-in');
+  body.classList.remove('is-logged-out');
+}
+
+function setLoggedOut() {
+  body.classList.add('is-logged-out');
+  body.classList.remove('is-logged-in');
+}
+
 setupMenu();
+checkAuth();
